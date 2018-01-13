83554a00a5498f0775662ca8d430f37456d46764
correctcpp <correctcpp@richelbilderbeek.nl>
2018-01-13 12:39:09 +0100

    Done exercise

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
-main(argc, argv) 
-{
+#include <iostream>
 
+/// A CLI application,
+/// that either says 'Hello' when
+/// the user has not supplied any CL arguments,
+/// else will show 'Hello', a space and the
+/// first (and only the first) supplied argument
+int main(int argc, char *argv[])
+{
+  std::cout << "Hello";
+  if (argc > 1)
+  {
+    std::cout << ' ' << argv[1];
+  }
+  std::cout << '\n';
 }
